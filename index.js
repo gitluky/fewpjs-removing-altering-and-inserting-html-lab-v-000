a20fa40c20dd900f94f8606f2a22916771d5aa1d
Jonathan Luk <jluk87@gmail.com>
2019-10-10 20:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 let element = document.getElementById('main')
 element.remove();
 
-let newHeader = document.createElement('h1').id;
+let newHeader = document.createElement('h1');
 newHeader.id = 'victory'
 document.body.appendChild(newHeader);
